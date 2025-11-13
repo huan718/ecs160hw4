040d41c57d3045d8a4f9c34f18b9c52f8c7c3930
chris-nguyen18 <chris.nguyensj04@gmail.com>
2025-11-12 17:09:24 -0800

    Part A excluding lazyload

--- persistence-framework/src/main/java/com/ecs160/persistence/RedisDB.java
+++ persistence-framework/src/main/java/com/ecs160/persistence/RedisDB.java
@@ -1,5 +1,6 @@
 package com.ecs160.persistence;
 
+import com.ecs160.persistence.annotations.*;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
@@ -15,7 +16,7 @@ import javassist.util.proxy.ProxyFactory;
 import redis.clients.jedis.Jedis;
 
 
-
+@PersistableObject
 public class RedisDB {
 
     private Jedis jedisSession;
@@ -24,14 +25,132 @@ public class RedisDB {
         jedisSession = new Jedis("localhost", 6379);;
     }
 
-
     public boolean persist(Object obj) {
-        return false;
+        Class<?> clazz = obj.getClass();
+        String className = clazz.getSimpleName();
+
+        try {
+            Field idField = null;
+            for (Field field : clazz.getDeclaredFields()) {
+                field.setAccessible(true);
+                if (field.isAnnotationPresent(Id.class)) {
+                    idField = field;
+                    break;
+                }
+            }
+
+            if (idField == null) {
+                System.err.println("No @Id field found.");
+                return false;
+            }
+            
+            Object idVal = idField.get(obj);
+            if (idVal == null) {
+                System.err.println("@Id cannot be null.");
+                return false; 
+            }
+
+            String redisKey = className + ":" + idVal.toString();
+
+            for (Field field : clazz.getDeclaredFields()) {
+                field.setAccessible(true);
+                if (field.isAnnotationPresent(PersistableField.class)) {
+                    Object value = field.get(obj);
+                    if (value != null) {
+                        // may need to change waiting on question for formatting
+                        // default to simple formatting for now
+                       jedisSession.hset(redisKey, field.getName(), value.toString());
+                    }
+                }
+            }
+
+            return true;
+
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
-
     public Object load(Object object)  {
-        return null;
+        Class <?> clazz = object.getClass();
+
+        if (!clazz.isAnnotationPresent(PersistableObject.class)) {
+            throw new RuntimeException("Class is not persistable");
+        }
+
+        try {
+            Field idField = null;
+            for (Field field : clazz.getDeclaredFields()) {
+                field.setAccessible(true);
+                if (field.isAnnotationPresent(Id.class)) {
+                    idField = field;
+                    break;
+                }
+            }
+
+            if (idField == null) {
+                System.err.println("No @Id field found.");
+                return false;
+            }
+
+            Object idVal = idField.get(object);
+            if (idVal == null) {
+                System.err.println("@Id cannot be null.");
+                return false; 
+            }
+
+            String string_idVal = idVal.toString();
+            String redisKey = clazz.getSimpleName() + ":" + string_idVal;
+
+            Map<String, String> redisField = jedisSession.hgetAll(redisKey);
+            if (redisField.isEmpty()) {
+                return null;
+            }
+
+            Object obj = null;
+            try {
+                obj = clazz.newInstance();
+                idField.set(obj, idVal); 
+            } catch (IllegalAccessException | InstantiationException e) {
+                e.printStackTrace();
+                return null;
+            }
+
+            for (Field field : clazz.getDeclaredFields()) {
+                field.setAccessible(true);
+                if (field.isAnnotationPresent(PersistableField.class)) {
+                    String value = redisField.get(field.getName());
+                    if (value != null) {
+                        field.set(obj, convertToFieldType(field.getType(), value));
+                    }
+                }
+            }
+
+            return obj;
+
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+            return null;
+        }     
+    }
+
+    private Object convertToFieldType(Class<?> type, String value) {
+        if (type == String.class) {
+            return value;
+        } else if (type == int.class || type == Integer.class) {
+            return Integer.parseInt(value);
+        } else if (type == long.class || type == Long.class) {
+            return Long.parseLong(value);
+        } else if (type == double.class || type == Double.class) {
+            return Double.parseDouble(value);
+        } else if (type == float.class || type == Float.class) {
+            return Float.parseFloat(value);
+        } else if (type == boolean.class || type == Boolean.class) {
+            return Boolean.parseBoolean(value);
+        } else {
+            return value;
+        }
     }
 
 }
---
+++ persistence-framework/src/main/java/com/ecs160/persistence/annotations/Id.java
@@ -0,0 +1,8 @@
+package com.ecs160.persistence.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Id {
+}
