be31b3e612429ae7f8a364740ec88422b7b2eed5
huan718 <huanyao2004@gmail.com>
2025-11-14 20:04:51 -0800

    part 2

---
+++ microservice-framework/src/main/java/com/ecs160/Endpointdef.java
@@ -0,0 +1,18 @@
+package com.ecs160;
+
+import java.lang.reflect.Method;
+import java.util.Objects;
+
+public final class Endpointdef {
+    private final Object instance;
+    private final Method method;
+    private final String url;
+    public Endpointdef(Object instance, Method method, String url){
+        this.instance = Objects.requireNonNull(instance);
+        this.method = Objects.requireNonNull(method);
+        this.url = Objects.requireNonNull(url);
+    }
+    public Object getInstance(){return instance;}
+    public Method getMethod(){return method;}
+    public String getUrl(){return url;}
+}
